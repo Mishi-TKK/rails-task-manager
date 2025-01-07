f91d0c7947e95b32408e3ffbd265013dbac9d5a4
Michelle Chege <169837635+Mishi-TKK@users.noreply.github.com>
2025-01-07 16:37:11 +0000

    User can add a new Task Challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,24 @@ class TasksController < ApplicationController
   def show
     @task =Task.find(params[:id])
   end
+  def new
+    @task=Task.new
+  end
+  def create
+    @task= Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Task successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+
+    end
+  end
+  def edit
+    @task = Task.find(params [ :id ])
+
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
